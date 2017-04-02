fb86901c398119a3ed55202d822410aed1294d8a
Artemser1 <artemkahramanov1999@gmail.com>
2017-04-02 17:12:22 +0300

    finalization and error correction

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,2 +1,21 @@
-console.log('1');
-console.log('1');
+$(document).ready(function(){
+
+$('.menu__butt').on('click', function(){
+	$('.active-menu').fadeIn(350).css('display', 'flex');
+});
+
+$('.request').on('click', function(){
+	$('.alert').fadeIn(350).css('display', 'flex');
+	var scroll_el = $('.butt__close');
+	if ($(scroll_el).length != 0) {
+		$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+	}
+});
+
+$('.butt__close').on('click', function(){
+	$('.active-menu').fadeOut(350);
+	$('.alert').fadeOut(350);
+});
+
+
+});
