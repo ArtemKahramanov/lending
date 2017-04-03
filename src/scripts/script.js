b9d43eb772541bbf76b4d65a133c072b2730087d
Artemser1 <artemkahramanov1999@gmail.com>
2017-04-03 22:58:10 +0300

    add section>torpfolio

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,15 +4,16 @@ $('.menu__butt').on('click', function(){
 	$('.active-menu').fadeIn(350).css('display', 'flex');
 });
 
-$('.request').on('click', function(){
+$('.button--request').on('click', function(event){
+	event.preventDefault();
 	$('.alert').fadeIn(350).css('display', 'flex');
-	var scroll_el = $('.butt__close');
+	var scroll_el = $('.button__close');
 	if ($(scroll_el).length != 0) {
 		$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
 	}
 });
 
-$('.butt__close').on('click', function(){
+$('.button__close').on('click', function(){
 	$('.active-menu').fadeOut(350);
 	$('.alert').fadeOut(350);
 });
